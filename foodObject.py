354a34389818cac8e30b250cbab31723e3d810c1
noahjepstein <noahjepstein@gmail.com>
2016-10-01 20:44:58 -0400

    Removed various out-of-date and unused code, added minor comments

--- foodObject.py
+++ foodObject.py
@@ -1,49 +1,63 @@
+# foodObject.py
+# Author: Noah J Epstein
+# Created: 4/1/2016
+# Modified: 4/4/2016
+
 import os
 import numpy as np
 # import vlc # only on osx
 
-class FoodItem: 
+
+# A FoodItem object has attributes:
+#	'RFIDTag' -- the rfid tag number of the tagged item
+#   'name' -- the name of the food
+# 	'foodGroup' -- the dinner plate food group of the item, i.e. grains, dairy etc.
+#   'healthIndex' -- the percieved healthiness of the food, on a scale from 1-10
+#   'soundFile' -- an mp3 file that holds an mp3 description of the food.
+#    appendToCSV(filename.CSV) -- appends the fooditem to a csv file
+#    saveToNewCSV(filename.csv) -- writes the foodItem to a csv file. overwrites an old csv. be careful.
+#    playSound() -- plays the sound file for the food item -- to be implemented
+class FoodItem:
 
 	def __init__(self, RFIDTag = -1, name = 'NOT A FOOD', foodGroup = 'NONE', healthIndex = -1, soundFile = 'NONE'):
 		self.RFIDTag = RFIDTag
 		self.name = name
 		self.foodGroup = foodGroup
 		self.healthIndex = healthIndex
-		
-		if (os.path.isfile(soundFile) and soundFile.endswith('.mp3')): 
+
+		if (os.path.isfile(soundFile) and soundFile.endswith('.mp3')):
 			self.soundFile = soundFile
-		elif soundFile == 'NONE': 
+		elif soundFile == 'NONE':
 			self.soundFile = soundFile
 		else:
 			self.soundFile = soundFile
 
 
-
-	def appendToCSV(self, filename): 
+	def appendToCSV(self, filename):
 
 		csvRow = (str(self.RFIDTag)     + ',' +
-			      str(self.name)        + ',' + 
-				  str(self.foodGroup)   + ',' + 
-				  str(self.healthIndex) + ',' + 
+			      str(self.name)        + ',' +
+				  str(self.foodGroup)   + ',' +
+				  str(self.healthIndex) + ',' +
 				  str(self.soundFile)   + '\n')
 
 		fd = open(filename,'a')
 		fd.write(csvRow)
 		fd.close()
 
-	def saveToNewCSV(self, filename): 	
+	def saveToNewCSV(self, filename):
 
 		csvRow = (str(self.RFIDTag)     + ',' +
-			      str(self.name)        + ',' + 
-				  str(self.foodGroup)   + ',' + 
-				  str(self.healthIndex) + ',' + 
+			      str(self.name)        + ',' +
+				  str(self.foodGroup)   + ',' +
+				  str(self.healthIndex) + ',' +
 				  str(self.soundFile)   + '\n')
 
 		fd = open(filename,'w')
 		fd.write(csvRow)
 		fd.close()
 
-	def CSVRowToFoodItem(self, row): 
+	def CSVRowToFoodItem(self, row):
 		rowArr = np.genfromtxt(row, delimiter = ",")
 		self.RFIDTag = rowArr[0]
 		self.name = rowArr[1]
@@ -55,14 +69,11 @@ class FoodItem:
 	def playSound(self):
 
 		if self.soundFile != 'NONE':
-			
-			soundfiledir = "/home/pi/foodGame/sounds/"		
+
+			soundfiledir = "/home/pi/foodGame/sounds/"
 			# for use on raspberry pi
 			bashstr = "omxplayer -o local " + soundfiledir + self.soundFile
 			os.system(bashstr)
 
-		else: 
+		else:
 			print "NOT IMPEMENTED"
-
-
-
--- mainGame.py
+++ mainGame.py
@@ -1,5 +1,10 @@
+# mainGame.py
+# Author: Noah J Epstein
+# Created: 4/1/2016
+# Modified: 9/30/2016
+
 import math
-import numpy as np 
+import numpy as np
 import csv
 import time
 import os
@@ -8,15 +13,14 @@ import pygame
 from RFID_TM import scanRFIDTag
 from foodObject import FoodItem
 
-
-# A FoodItem object has attributes: 
+# A FoodItem object has attributes:
 #	'RFIDTag' -- the rfid tag number of the tagged item
 #   'name' -- the name of the food
-# 	'foodGroup' -- the dinner plate food group of the item, i.e. grains, dairy etc. 
+# 	'foodGroup' -- the dinner plate food group of the item, i.e. grains, dairy etc.
 #   'healthIndex' -- the percieved healthiness of the food, on a scale from 1-10
-#   'soundFile' -- an mp3 file that holds an mp3 description of the food. 
+#   'soundFile' -- an mp3 file that holds an mp3 description of the food.
 #   appendToCSV(filename.CSV) -- appends the fooditem to a csv file
-#   saveToNewCSV(filename.csv) -- writes the foodItem to a csv file. overwrites an old csv. be careful. 
+#   saveToNewCSV(filename.csv) -- writes the foodItem to a csv file. overwrites an old csv. be careful.
 #   playSound() -- plays the sound file for the food item -- to be implemented
 
 ########### GPIO HANDLING and CONSTANTS ##################
@@ -32,7 +36,6 @@ TIME_DIFF_MIN = 10000
 
 resetClock = pygame.time.Clock()
 checkClock = pygame.time.Clock()
-
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -50,10 +53,10 @@ foodGroups = []
 #os.system('omxplayer -o local /home/pi/foodGame/sounds/Windows*')
 GPIO.output(READY_LIGHT_OUTPUT_CHAN, True)
 
-# a bunch of funcs for doing random tasks, interrupt handling, etc. 
+# a bunch of funcs for doing random tasks, interrupt handling, etc.
 
 def loadFoodList():
-	
+
 	foodGroupsFile = '/home/pi/foodGame/validFoodGroups.csv'
 	f = open(foodGroupsFile, 'rb')
 	reader = csv.reader(f)
@@ -66,48 +69,48 @@ def loadFoodList():
 	reader = csv.reader(f)
 	foodListRaw = list(reader)
 	f.close()
-	
-	for row in foodListRaw: 
-		
-		food = FoodItem( RFIDTag = str(row[0]), 
-						 name = str(row[1]), 
-						 foodGroup = str(row[2]), 
-						 healthIndex = str(row[3]), 
+
+	for row in foodListRaw:
+
+		food = FoodItem( RFIDTag = str(row[0]),
+						 name = str(row[1]),
+						 foodGroup = str(row[2]),
+						 healthIndex = str(row[3]),
 						 soundFile = str(row[4]))
-		
+
 		foodList[food.RFIDTag] = food
 
 	return foodList
 
-def balancedPlate(): 
+def balancedPlate():
 
-	# if foodList.foodGroups contains one of every food group in foodGroups, 
+	# if foodList.foodGroups contains one of every food group in foodGroups,
 	# plate is balanced (i.e. the child has created a balanced meal)
 	# otherwise they must try again
 
 	currFoodGroupList = [food.foodGroup for food in currentPlate.values()]
-		
 
-	for food in currentPlate.values():		 
+
+	for food in currentPlate.values():
 		print str(food.name) + " " + str(food.foodGroup)
-		if food.foodGroup not in foodGroups[0]: 
+		if food.foodGroup not in foodGroups[0]:
 			print "food is not part of a valid food group"
 			return False
 
-	if not all(group in currFoodGroupList for group in foodGroups[0]): 
+	if not all(group in currFoodGroupList for group in foodGroups[0]):
 		print "need to have all food groups represented on plate!"
 		return False
 
 	groupCounts = collections.Counter(currFoodGroupList)
 	if groupCounts['fruit'] not == 2 or groupCounts['fruit'] not == 3:
 		return False
-	elif groupCounts['vegetable'] not == 3: 
+	elif groupCounts['vegetable'] not == 3:
 		return False
 	elif groupCounts['protein'] > 2:
 		return False
-	elif groupCounts['dairy'] > 2: 
+	elif groupCounts['dairy'] > 2:
 		return False
-	elif groupCounts['treat'] > 1: 
+	elif groupCounts['treat'] > 1:
 		return False
 	elif groupCounts['grain']  not == 3 and groupCounts['grain'] not == 4:
 		return False
@@ -115,36 +118,38 @@ def balancedPlate():
 	return True
 
 
+# CALLBACKS AND I/O MANIPULATION
+
 def resetCallback(channel):
 
 	global resetClock
-	
+
 	timeDiff = resetClock.tick()
 	print timeDiff
-	
-	if timeDiff > TIME_DIFF_MIN: 
+
+	if timeDiff > TIME_DIFF_MIN:
 
 		print "Reset detected."
-		os.system("omxplayer -o local /home/pi/foodGame/sounds/new\ game.mp3") 
+		os.system("omxplayer -o local /home/pi/foodGame/sounds/new\ game.mp3")
 		resetPlate()
 
 
-def gameCheckCallback(channel): 
+def gameCheckCallback(channel):
 
 	global checkClock
 	timeDiff = checkClock.tick()
 	print timeDiff
-	if timeDiff > TIME_DIFF_MIN: 
+	if timeDiff > TIME_DIFF_MIN:
 
 		print "Checking state of current game: "
 
-		if balancedPlate(): 
+		if balancedPlate():
 			print "success!"
 			triggerSolenoid()
 			os.system("omxplayer -o local /home/pi/foodGame/sounds/cheer.mp3")
 			# make success sound
 			resetPlate()
-		else: 
+		else:
 			# make buzzer sound
 			os.system("omxplayer -o local /home/pi/foodGame/sounds/buzzer.mp3")
 			return
@@ -153,11 +158,11 @@ def gameCheckCallback(channel):
 		# trigger gpio pin for solenoid latch
 
 
-def triggerSolenoid(): 
+def triggerSolenoid():
 
 	print "Triggering solenoid."
 	GPIO.output(SOL_TRIGGER_OUTPUT, True)
-	time.sleep(1.000) # wait 1000 ms 
+	time.sleep(1.000) # wait 1000 ms
 	GPIO.output(SOL_TRIGGER_OUTPUT, False)
 
 def resetPlate():
@@ -165,11 +170,11 @@ def resetPlate():
 	currentPlate.clear()
 	print "after call to currentPlate"
 
-######## MAIN GAME CODE #########
+############################## MAIN GAME LOOP ##################################
 
 foodList = loadFoodList()
 
-while not RESET_FLAG: 
+while not RESET_FLAG:
 
 	GPIO.add_event_detect(RESET_INPUT_CHAN, GPIO.RISING, callback = resetCallback, bouncetime = 1000)
 	GPIO.add_event_detect(CHECK_GAME_INPUT_CHAN, GPIO.RISING, callback = gameCheckCallback, bouncetime = 1000)
@@ -177,34 +182,34 @@ while not RESET_FLAG:
 	newID = scanRFIDTag()
 
 	if foodList.has_key(newID):
-		foodList[newID].playSound() 
-		if not currentPlate.has_key(newID): 
+		foodList[newID].playSound()
+		if not currentPlate.has_key(newID):
 			currentPlate[newID] = foodList[newID]
 			print "adding " + foodList[newID].name + " to list"
-		else: 
+		else:
 			del currentPlate[newID]
 			print "removing " + foodList[newID].name + " from list"
 
 
-	else: 
+	else:
 		os.system("omxplayer -o local /home/pi/foodGame/sounds/fart.mp3")
 		print "Food IDN " + newID + "  not recognized!"
 
 	# compare to list of tags to get a relevant food object
 	# add the food object to currentPlate
 	# check if current state is healthy meal state
-	# can scan object again to remove it 
+	# can scan object again to remove it
 
-	# for ID, food in currentPlate.iteritems(): 
+	# for ID, food in currentPlate.iteritems():
 	# 	print food.name
 
-	if balancedPlate(): 
+	if balancedPlate():
 		print "Plate currently balanced."
-	else: 
+	else:
 		print "Plate not balanced."
 
 	GPIO.remove_event_detect(RESET_INPUT_CHAN)
 	GPIO.remove_event_detect(CHECK_GAME_INPUT_CHAN)
-	
+
 print foodList
 print currentPlate
